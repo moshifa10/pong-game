60486189e57ce7f187fb7f22fd70ba7f91c850da
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-01-07 21:22:43 +0200

    managed to make it to detect collisions

--- ball.py
+++ ball.py
@@ -3,16 +3,27 @@ from turtle import Turtle
 
 class Ball(Turtle):
     def __init__(self,width, height, color):
-        super().__init__()
+        super().__init__() 
         self.shape("circle")
         self.color(color)
         self.penup()
         self.shapesize(stretch_len= width,stretch_wid=height)
         self.speed(0)
+        self.x_move = 10
+        self.y_move = 10
 
     
     def move(self):
         # get x_cor and y_core and increment both by 20
         x_cor, y_cor = self.position()
-        self.goto(x_cor + 20, y_cor + 20)
-        
+        self.goto(x_cor + self.x_move, y_cor + self.y_move)
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def reset(self):
+        self.goto(0,0)
+        self.bounce_x()
--- pong_game.py
+++ pong_game.py
@@ -28,12 +28,12 @@ screen.title("Pong Game")
 screen.tracer(0)
 
 # right Padel starting position 
-X_POS = -380
+X_POS = 372
 Y_POS = 0
 padel1_r = Padel1(X_POS, Y_POS)
 
 # Left Padel starting position
-X_POS_padel_left = 372
+X_POS_padel_left = -380
 Y_POS_padel_left = 0
 padel2_l = Padel2(X_POS_padel_left, Y_POS_padel_left)
 
@@ -42,17 +42,33 @@ ball = Ball(1,1, "red")
 
 # Make the screen listen to moves
 screen.listen()
-screen.onkey(fun=padel1_r.down, key="s")
-screen.onkey(fun=padel1_r.up, key="w")
-screen.onkey(fun=padel2_l.down, key="Down")
-screen.onkey(fun=padel2_l.up, key="Up")
+screen.onkey(fun=padel1_r.down, key="Down")
+screen.onkey(fun=padel1_r.up, key="Up")
+screen.onkey(fun=padel2_l.down, key="s")
+screen.onkey(fun=padel2_l.up, key="w")
 
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(0.07)
     ball.move()
-    
+
+    x_cor , y_cor = ball.xcor(), ball.ycor()
+    if y_cor > 280 or y_cor < -280:
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(padel1_r.all_head[0]) < 50 and ball.xcor() > 340 or  ball.distance(padel2_l.all_head[0]) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
+
+    # Detect r_paddle miss
+    if ball.xcor() > 380:
+        ball.reset()
+
+    # Detect l_paddle miss
+    if ball.xcor() < -380:
+        ball.reset()
+
 
 screen.exitonclick()
